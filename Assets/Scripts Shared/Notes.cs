3509fec5a5179b7fd230b19cedb58855c08da8ad
rod <rod@roddickinson.net>
2021-01-23 23:14:33 +0000

    creating training scene

--- Assets/Scripts Shared/Notes.cs
+++ Assets/Scripts Shared/Notes.cs
@@ -7,7 +7,8 @@
 
 // start ML agents
 // cd to config folder
-// mlagents-learn trainer_config_push_cube.yaml --run-id=PushBlockFourteen --force
+// mlagents-learn trainer_config_push_cube.yaml --run-id=Agent-1-1 --force
+// naming protocol run-id=Agent-<no>-<iteration>
 
 // ADJUSTMENTS
 // Changed the cube from mass 1, Drag 0.5, TO mass 1.5, Drag 1  - to make it harder for the agent
